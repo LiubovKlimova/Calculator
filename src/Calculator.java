3779d35854086c56954afc9da4c10fef3e628294
Liubov Klimova <luibovklimova@gmail.com>
2019-01-19 12:36:55 -0500

    add subtruct method

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,6 +4,9 @@ public class Calculator {
         double sum = 0;
         sum = num1 + num2;
         return sum;
+    }
 
+    public static double substruct( double num1, double num2){
+        return num1 - num2;
     }
 }
